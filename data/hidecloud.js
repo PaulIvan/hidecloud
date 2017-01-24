02374148ad1c0403302dac5b6a9c865f8da04512
Paul Ivan <paul.alex.ivan@gmail.com>
2017-01-24 23:27:02 +0100

    Added Cloud storage using firebase.
    It is now possible to hide, unhide tracks and all changes are synced.
    
    Chrome extension icon is now always active

--- data/hidecloud.js
+++ data/hidecloud.js
@@ -1,223 +1,249 @@
-//Set ext scope variables
-var hiddenTracks = localStorage.hiddenTracks ? JSON.parse(localStorage.hiddenTracks) : {};
-// details about the current track
-var trackInfo = {
-	element: {},
-	title: "",
-	href: ""
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDJ0p-fX25-LkS_muYw57-O2l45qpUyxSM",
+	authDomain: "hidecloud-294e7.firebaseapp.com",
+	databaseURL: "https://hidecloud-294e7.firebaseio.com",
+	storageBucket: "hidecloud-294e7.appspot.com",
+	messagingSenderId: "26452456408"
 };
+firebase.initializeApp(config);
 
-var stream = [];
-
-var songList = document.createElement('ul');
-
-if (jQuery) {
-
-	$(document).ready(function(){
-		$(songList).addClass('hidden-songs');
-		$('body').append(songList);
-		showBtn();
-
-		var unhideAllBtn = document.createElement('a');
-		$(unhideAllBtn).addClass("unhide-all");
-		$('body').append(unhideAllBtn);
-
-		$(unhideAllBtn).on('open', function() { $(this).show() });
-		$(unhideAllBtn).on('close', function() { $(this).hide() });
-		$(unhideAllBtn).on('click', unhideAllTracks);
-
-		//On click of hide track button, execute hide track function
-		$('body').on('click', 'a.hide-track', function() {
-			hideTrack($(this));
-		});
-		
-		$('body').on('click', 'a.unhide-track', function() {
-			unhideTrack($(this).data('href'), $(this).data('track'));
-		});
-
-		$('.playControls .skipControl__previous').on('click', function() { trackInfo.skipBack = true; });
+var database = firebase.database();
+var cloudTracks = database.ref('/tracks');
 
 
-		$(document).on('lazyLoad', function(e) {
-			$(".soundList__item")
-				.toArray()
-				.filter(function(node) { return stream.indexOf(node) === -1; })
-				.forEach(function(node) { processTrack(node); });
-		});
 
-		if (Object.keys(hiddenTracks).length > 0) {			
-			$('.show-hidden').fadeIn();
-			populateHiddenList();
-		}
+//Set ext scope variables
+var hiddenTracks = {};
 
-		$(document).trigger('lazyLoad');
-	});
+cloudTracks.once('value').then(function (snapshot) {
+	hiddenTracks = JSON.parse(snapshot.val());
 
-	// Either add a hide button to the track, or hide the track if localStorage deems it hidden
-	function processTrack(trackNode) {
-		var trackDetails = $(trackNode).find("a.soundTitle__title").first(),
-			buttonGroup = $(trackNode).find('div.sc-button-group').first(),
-			trackHref = $(trackDetails).attr("href"),
-			songName = $(trackDetails).find("span").text();
+	// details about the current track
+	var trackInfo = {
+		element: {},
+		title: "",
+		href: ""
+	};
 
-		if(isTrackHidden(trackHref)) {
-			$(trackNode).hide();
-		} else if ($(buttonGroup).find('.hide-track').length == 0) {
-			$(buttonGroup).append("<a class='sc-button hide-track sc-button-small sc-button-responsive' data-track='"+ songName +"' data-href='" + trackHref + "' title='Hide This Track'>HIDE</a>").attr("href","");
+	var stream = [];
 
-			stream.push(trackNode);
-		}
-	}
+	var songList = document.createElement('ul');
 
-	function countVisibleTracks() {
-		return $('.soundList__item:visible').length;
-	}
-	
-	function showBtn() {
-		if ($('.show-hidden') != '') { 
-			var showHiddenBtn = document.createElement('a');
-			$(showHiddenBtn).addClass('show-hidden');			
-			showHiddenBtn.text = "You have hidden tracks! Click to see them";
-			
-			$('body').append(showHiddenBtn);
-		}
-	}
+	if (jQuery) {
 
-	$('body').on('click', '.show-hidden', function() {
-		$(this).text() === 'You have hidden tracks! Click to see them' ? showSongList() : hideSongList();
-	});
+		$(document).ready(function () {
+			$(songList).addClass('hidden-songs');
+			$('body').append(songList);
+			showBtn();
 
-	function showSongList() {
-		$('.hidden-songs').slideDown();
-		$('.show-hidden').text('Hide this list!');
-		$('a.unhide-all').trigger('open');
-	}
-	
-	function hideSongList() {
-		$('.hidden-songs').slideUp();
-		$('.show-hidden').text('You have hidden tracks! Click to see them');
-		$('a.unhide-all').trigger('close');
-	}
+			var unhideAllBtn = document.createElement('a');
+			$(unhideAllBtn).addClass("unhide-all");
+			$('body').append(unhideAllBtn);
 
-	function unhideAllTracks() {
-		for(href in hiddenTracks) {
-			unhideTrack(href, hiddenTracks[href]);
-		}
-	}
+			$(unhideAllBtn).on('open', function () { $(this).show() });
+			$(unhideAllBtn).on('close', function () { $(this).hide() });
+			$(unhideAllBtn).on('click', unhideAllTracks);
 
-	//Hides the track and adds the track name to the localStorage object
-	function hideTrack(e) {
-		var href, listItem, songName;
-		href = e.attr('data-href');
+			//On click of hide track button, execute hide track function
+			$('body').on('click', 'a.hide-track', function () {
+				hideTrack($(this));
+			});
 
-		songName = e.attr('data-track');
-		listItem = e.closest('li.soundList__item');
+			$('body').on('click', 'a.unhide-track', function () {
+				unhideTrack($(this).data('href'), $(this).data('track'));
+			});
 
-		hiddenTracks[href] = songName;
-		stream = stream.filter(function(node) { return node !== listItem });
+			$('.playControls .skipControl__previous').on('click', function () { trackInfo.skipBack = true; });
 
-		listItem.slideUp();
-		localStorage.hiddenTracks = JSON.stringify(hiddenTracks);
-		populateHiddenList();
 
-		$('a.show-hidden').slideDown();
+			$(document).on('lazyLoad', function (e) {
+				$(".soundList__item")
+					.toArray()
+					.filter(function (node) { return stream.indexOf(node) === -1; })
+					.forEach(function (node) { processTrack(node); });
+			});
 
-		if (trackInfo.href == href) {
-			nextSong();
-		}
-
-		// If there is only 5 visible tracks left, try loading more
-		countVisibleTracks() <= 5 ? loadMoreTracks() : 0;
-	}
-	
-	function unhideTrack(href, songName) {
-		var track = $('[aria-label="'+songName+'"]');
-		track.closest('.soundList__item').slideDown();
-		delete hiddenTracks[href];
-
-		localStorage.hiddenTracks = JSON.stringify(hiddenTracks);
-		populateHiddenList();
-	
-		if (Object.keys(hiddenTracks).length == 0) {
-			hideSongList();
-			$('a.show-hidden').slideUp();
-		}
-	}
-	
-	function populateHiddenList() {
-		$(songList).html('');
-	
-		for (var href in hiddenTracks) {
-			var songName = hiddenTracks[href];
-			var li = document.createElement('li');
-			var unhideBtn = document.createElement('a');
-			
-			$(li).text(songName);
-			$(unhideBtn).text("UNHIDE").addClass('unhide-track').attr('style', 'color: red; float: right; cursor: pointer;').attr('data-href', href).attr('data-track', songName);
-			$(li).append(unhideBtn);
-
-			$(songList).append(li);				
-		}
-	}
-
-	// Fires off a tiny scroll animation to trigger the stream lazy load
-	function loadMoreTracks() {
-		$('html, body').animate({scrollTop: $(window).scrollTop() + 10}, 10);
-	}
-	
-	function isTrackHidden(trackUrl) {
-		return (trackUrl && parsePlaylistHref(trackUrl) in hiddenTracks)
-	}
-
-	function parsePlaylistHref(href) {
-		return href.indexOf("?in=") > -1 ? (href.split("?in=")[1][0] !== '/') ? '/' + href.split("?in=")[1] : href.split("?in=")[1] : href;
-	}
-
-	function skipControl() {
-		return trackInfo.skipBack ? $('.playControls .skipControl__previous') : $('.playControls .skipControl__next');
-	}
-	
-	function nextSong() {
-		$('.skipControl__next').click();
-	}
-
-	
-	var playingObserver = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-
-			trackInfo.element = document.querySelector('.playbackSoundBadge__titleLink');
-
-			// Only check hidden tracks if songTitle is a descendant
-			// of the mutation target element.
-			if($.contains(mutation.target, trackInfo.element)) {
-				trackInfo.href = $(trackInfo.element).attr('href');
-
-				if(isTrackHidden(trackInfo.href)) {
-					skipControl().trigger("click");
-				} else {
-					trackInfo.skipBack = false;
+			cloudTracks.on('value', function (snapshot) {
+				hiddenTracks = JSON.parse(snapshot.val());
+				if (Object.keys(hiddenTracks).length > 0) {
+					$('.show-hidden').fadeIn();
+					populateHiddenList();
 				}
-			}
+				stream = [];
+				$(document).trigger('lazyLoad');
+			});
 		});
-	});
 
-	// Watches for node additions to the lazyLoad list
-	var lazyLoadObserver = new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			if(mutation.type === "childList" && mutation.addedNodes.length) {
-				$(document).trigger("lazyLoad");
+		// Either add a hide button to the track, or hide the track if localStorage deems it hidden
+		function processTrack(trackNode) {
+			var trackDetails = $(trackNode).find("a.soundTitle__title").first(),
+				buttonGroup = $(trackNode).find('div.sc-button-group').first(),
+				trackHref = $(trackDetails).attr("href"),
+				songName = $(trackDetails).find("span").text();
+
+			if (isTrackHidden(trackHref)) {
+				$(trackNode).hide();
+			} else if ($(buttonGroup).find('.hide-track').length == 0) {
+				$(buttonGroup).append("<a class='sc-button hide-track sc-button-small sc-button-responsive' data-track='" + songName + "' data-href='" + trackHref + "' title='Hide This Track'>HIDE</a>").attr("href", "");
+
+				stream.push(trackNode);
 			}
+		}
+
+		function countVisibleTracks() {
+			return $('.soundList__item:visible').length;
+		}
+
+		function showBtn() {
+			if ($('.show-hidden') != '') {
+				var showHiddenBtn = document.createElement('a');
+				$(showHiddenBtn).addClass('show-hidden');
+				showHiddenBtn.text = "You have hidden tracks! Click to see them";
+
+				$('body').append(showHiddenBtn);
+			}
+		}
+
+		$('body').on('click', '.show-hidden', function () {
+			$(this).text() === 'You have hidden tracks! Click to see them' ? showSongList() : hideSongList();
 		});
-	});
 
-	var lazyLoadList = document.querySelector('div.lazyLoadingList');
-	var currentPlaying = document.querySelector('.playControls');
+		function showSongList() {
+			$('.hidden-songs').slideDown();
+			$('.show-hidden').text('Hide this list!');
+			$('a.unhide-all').trigger('open');
+		}
 
-	playingObserver.observe(currentPlaying , { childList: true, characterData: true, subtree : true });
-	lazyLoadObserver.observe(lazyLoadList, { childList: true, subtree: true });
+		function hideSongList() {
+			$('.hidden-songs').slideUp();
+			$('.show-hidden').text('You have hidden tracks! Click to see them');
+			$('a.unhide-all').trigger('close');
+		}
+
+		function unhideAllTracks() {
+			for (href in hiddenTracks) {
+				unhideTrack(href, hiddenTracks[href]);
+			}
+		}
+
+		//Hides the track and adds the track name to the localStorage object
+		function hideTrack(e) {
+			var href, listItem, songName;
+			href = e.attr('data-href');
+
+			songName = e.attr('data-track');
+			listItem = e.closest('li.soundList__item');
+
+			hiddenTracks[href] = songName;
+			stream = stream.filter(function (node) { return node !== listItem });
+
+			listItem.slideUp();
+			//localStorage.hiddenTracks = JSON.stringify(hiddenTracks);
+			cloudTracks.set(JSON.stringify(hiddenTracks));
+
+			populateHiddenList();
+
+			$('a.show-hidden').slideDown();
+
+			if (trackInfo.href == href) {
+				nextSong();
+			}
+
+			// If there is only 5 visible tracks left, try loading more
+			countVisibleTracks() <= 5 ? loadMoreTracks() : 0;
+		}
+
+		function unhideTrack(href, songName) {
+			var track = $('[aria-label="' + songName + '"]');
+			track.closest('.soundList__item').slideDown();
+			delete hiddenTracks[href];
+
+			//localStorage.hiddenTracks = JSON.stringify(hiddenTracks);
+			cloudTracks.set(JSON.stringify(hiddenTracks));
+
+			populateHiddenList();
+
+			if (Object.keys(hiddenTracks).length == 0) {
+				hideSongList();
+				$('a.show-hidden').slideUp();
+			}
+		}
+
+		function populateHiddenList() {
+			$(songList).html('');
+
+			for (var href in hiddenTracks) {
+				var songName = hiddenTracks[href];
+				var li = document.createElement('li');
+				var unhideBtn = document.createElement('a');
+
+				$(li).text(songName);
+				$(unhideBtn).text("UNHIDE").addClass('unhide-track').attr('style', 'color: red; float: right; cursor: pointer;').attr('data-href', href).attr('data-track', songName);
+				$(li).append(unhideBtn);
+
+				$(songList).append(li);
+			}
+		}
+
+		// Fires off a tiny scroll animation to trigger the stream lazy load
+		function loadMoreTracks() {
+			$('html, body').animate({ scrollTop: $(window).scrollTop() + 10 }, 10);
+		}
+
+		function isTrackHidden(trackUrl) {
+			return (trackUrl && parsePlaylistHref(trackUrl) in hiddenTracks)
+		}
+
+		function parsePlaylistHref(href) {
+			return href.indexOf("?in=") > -1 ? (href.split("?in=")[1][0] !== '/') ? '/' + href.split("?in=")[1] : href.split("?in=")[1] : href;
+		}
+
+		function skipControl() {
+			return trackInfo.skipBack ? $('.playControls .skipControl__previous') : $('.playControls .skipControl__next');
+		}
+
+		function nextSong() {
+			$('.skipControl__next').click();
+		}
+
+		var playingObserver = new MutationObserver(function (mutations) {
+			mutations.forEach(function (mutation) {
+
+				trackInfo.element = document.querySelector('.playbackSoundBadge__titleLink');
+
+				// Only check hidden tracks if songTitle is a descendant
+				// of the mutation target element.
+				if ($.contains(mutation.target, trackInfo.element)) {
+					trackInfo.href = $(trackInfo.element).attr('href');
+
+					if (isTrackHidden(trackInfo.href)) {
+						skipControl().trigger("click");
+					} else {
+						trackInfo.skipBack = false;
+					}
+				}
+			});
+		});
+
+		// Watches for node additions to the lazyLoad list
+		var lazyLoadObserver = new MutationObserver(function (mutations) {
+			mutations.forEach(function (mutation) {
+				if (mutation.type === "childList" && mutation.addedNodes.length) {
+					$(document).trigger("lazyLoad");
+				}
+			});
+		});
+
+		var lazyLoadList = document.querySelector('div.lazyLoadingList');
+		var currentPlaying = document.querySelector('.playControls');
+
+		playingObserver.observe(currentPlaying, { childList: true, characterData: true, subtree: true });
+		lazyLoadObserver.observe(lazyLoadList, { childList: true, subtree: true });
 
 
-} else {
-	console.log('Hidecloud is not active');
-}
+	} else {
+		console.log('Hidecloud is not active');
+	}
 
+})
